6d5d52e8d39858fed621b64510f0dc6b2f1a95ba
fares8lefi <135213818+fares8lefi@users.noreply.github.com>
2025-03-16 15:22:46 +0100

    routing functionality for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Navbar from './components/Navbar';
 import Footer  from './components/Footer';
 import Login from './layouts/Login'
 import Singup from './layouts/Singup.jsx'
+import Error  from './layouts/Error.jsx'
+import UsersTable from './components/UsersTable.jsx'
+import CarCards from './components/CarCards.jsx'
+import CarDetaille from './layouts/CarDetaille.jsx'
 function App() {
   return (
     <>
@@ -12,7 +16,10 @@ function App() {
         <Route path="/Footer" element={<Footer />} />
         <Route path="/login" element={<Login />} />
         <Route path="/singup" element={<Singup />} />
-
+        <Route path="/UsersTable" element={<UsersTable />} />
+        <Route path="/carCards" element={<CarCards />} />
+        <Route path='/carDetaille/:id' element={<CarDetaille/>}></Route>
+        {/*<Route path='/*' element={<Error/>}></Route>*/}
       </Routes>
     </>
   )
--- src/RouteComp.js
+++ src/RouteComp.js
@@ -2,8 +2,12 @@ import React from 'react'
 import {Routes, Route} from 'react-router-dom';
 import App from './App.jsx'
 import Navbar from './components/Navbar.jsx'
+import UsersTable from './components/UsersTable.jsx'
+import CarCards from './components/CarCards.jsx'
 import Login from './layouts/Login.jsx'
 import Singup from './layouts/Singup.jsx'
+import Error  from './layouts/Error.jsx'
+import CarDetaille  from './layouts/CarDetaille.jsx'
 export default function RouteComp() {
   return (
     <div>
@@ -12,6 +16,10 @@ export default function RouteComp() {
         <Route path='/Navbar' element={<Navbar/>}></Route>
         <Route path='/login' element={<Login/>}></Route>
         <Route path='/singup' element={<Singup/>}></Route>
+        <Route path='/usersTable' element={<UsersTable/>}></Route>
+        <Route path='/carCards' element={<CarCards/>}></Route>
+        <Route path='/carDetaille/:id' element={<CarDetaille/>}></Route>
+        {/*<Route path='/*' element={<Error/>}></Route>*/}
       </Routes>
 
     </div>
