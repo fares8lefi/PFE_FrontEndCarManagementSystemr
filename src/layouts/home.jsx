239a66cf772088ecf92f1e3fca2a6bb0a1bb0555
fares8lefi <135213818+fares8lefi@users.noreply.github.com>
2025-03-25 22:53:14 +0100

    create home page

---
+++ src/layouts/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import CarCards from '../components/CarCards';
+export default function home() {
+  return (
+    <div>
+    <Navbar/>
+    <CarCards/>
+     <Footer/>
+    </div>
+  )
+}
