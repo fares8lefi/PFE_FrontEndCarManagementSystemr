dd6dcd616375521590f6c34348f61f2b3e5d2839
fares8lefi <135213818+fares8lefi@users.noreply.github.com>
2025-03-25 15:48:36 +0100

    Implement API fonctionality "addComment"

--- src/services/ApiComment.js
+++ src/services/ApiComment.js
@@ -5,3 +5,13 @@ const apiurl = "http://localhost:3000/comment";
 export async function getCommentsByCar(id) {
     return await axios.get(`${apiurl}/getCommentsByCar/${id}`);
 }
+
+export async function addComment(carId, content) {
+    return await axios.post(
+      `${apiurl}/addComment`,
+      { carId, content },
+      {
+        withCredentials: true
+      }
+    );
+  }
