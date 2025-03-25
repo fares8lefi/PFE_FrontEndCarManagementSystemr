6d130f7271414a663a5b16db59eb564c6b3a594e
fares8lefi <135213818+fares8lefi@users.noreply.github.com>
2025-03-25 22:59:38 +0100

    create home page route

--- src/RouteComp.js
+++ src/RouteComp.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Routes, Route} from 'react-router-dom';
+import {BrowserRouter,Routes, Route} from 'react-router-dom';
 import App from './App.jsx'
 import Navbar from './components/Navbar.jsx'
 import UsersTable from './components/UsersTable.jsx'
@@ -7,12 +7,14 @@ import CarCards from './components/CarCards.jsx'
 import Login from './layouts/Login.jsx'
 import Singup from './layouts/Singup.jsx'
 // import Error  from './layouts/Error.jsx'
+import home  from './layouts/home.jsx'
 import CarDetaille  from './layouts/CarDetaille.jsx'
 import CarPlusDetaille  from './components/CarPlusDetaille.jsx'
 import CommentCards  from './components/CommentCards.jsx'
 export default function RouteComp() {
   return (
     <div>
+       <BrowserRouter> 
       <Routes>
         <Route path='/App' element={<App/>}></Route>
         <Route path='/Navbar' element={<Navbar/>}></Route>
@@ -23,9 +25,10 @@ export default function RouteComp() {
         <Route path='/carDetaille/:id' element={<CarDetaille/>}></Route>
         <Route path='/carPlusDetaille/:id' element={<CarPlusDetaille/>}></Route>
         <Route path='/commentCards/:id' element={<CommentCards/>}></Route>
+        <Route path='/*' element={<home/>}></Route>
         {/*<Route path='/*' element={<Error/>}></Route>*/}
       </Routes>
-
+      </BrowserRouter> 
     </div>
   )
 }
