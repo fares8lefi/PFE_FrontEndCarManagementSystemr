04049ecbe17b31625412bec5b2c5a224c95b1ee3
fares8lefi <135213818+fares8lefi@users.noreply.github.com>
2025-03-29 17:52:43 +0100

    Apis for favoris

--- src/services/ApiFavoris.js
+++ src/services/ApiFavoris.js
@@ -2,6 +2,17 @@ import axios from "axios";
 
 const apiurl = "http://localhost:3000/favoris"; 
 
-export async function getUserFavorites(id) {
-    return await axios.get(`${apiurl}/getUserFavorites/${id}`);
+export async function getUserFavorites() {
+    return await axios.get(`${apiurl}/getUserFavorites`, {
+      withCredentials: true 
+    });
+  }
+
+export async function addCarToFavorites(carId) {
+    return await axios.post(`${apiurl}/addCarToFavorites`,{carId},
+        {
+            withCredentials: true,
+        }
+    );
 }
+
