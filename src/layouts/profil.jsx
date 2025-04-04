405a40075ca9cf8e0bfdc93aa1b463056dc91971
fares8lefi <135213818+fares8lefi@users.noreply.github.com>
2025-04-04 19:34:55 +0200

    create profil page

---
+++ src/layouts/profil.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+import ProfilCard from '../components/ProfilCards';
+export default function profil() {
+  return (
+    <div>
+        <Navbar/>
+        <ProfilCard/>
+        
+    </div>
+  )
+}
