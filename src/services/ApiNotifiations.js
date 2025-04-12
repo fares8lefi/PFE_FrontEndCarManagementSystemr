5ea09853fd2c59a6510b4b554866527875e89b42
fares8lefi <135213818+fares8lefi@users.noreply.github.com>
2025-04-12 20:09:52 +0200

    Update  getUserNotifications

--- src/services/ApiNotifiations.js
+++ src/services/ApiNotifiations.js
@@ -3,10 +3,14 @@ import axios from "axios";
 const apiurl = "http://localhost:3000/notification"; 
 
 export const getUserNotifications = () => {
-    return axios.get(`${apiurl}/getUserNotifications`,{
-      withCredentials: true,
-    });
-  };
+  return axios.get(`${apiurl}/getUserNotifications`, {
+    withCredentials: true,
+    headers: {
+      "Cache-Control": "no-cache",
+    },
+  });
+};
+
 
   export const markAsRead = () => {
     return axios.put(`${apiurl}/markAsRead`, {}, {
