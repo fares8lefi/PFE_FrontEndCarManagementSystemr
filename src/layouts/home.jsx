605da69243e0b637d9d5b02e8bc80e162bbb2fd2
fares8lefi <135213818+fares8lefi@users.noreply.github.com>
2025-04-14 21:19:06 +0200

    Implement Contact and Services sections on the Home page

--- src/layouts/home.jsx
+++ src/layouts/home.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import CarCards from '../components/CarCards';
+import Services from './Services';
+import Contact from './Contact';
 export default function home() {
   return (
     <div>
     <Navbar/>
     <CarCards/>
+    <Services/>
+    <Contact/>
      <Footer/>
     </div>
   )
